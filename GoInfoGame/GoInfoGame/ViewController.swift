06f1942562129db45d5a28b44dc5e8f1c3f79904
Achyut Kumar M <achyutkumar.m@gmail.com>
2023-11-15 19:08:35 +0530

    call overpassManager function to get nwr count

--- GoInfoGame/GoInfoGame/ViewController.swift
+++ GoInfoGame/GoInfoGame/ViewController.swift
@@ -8,11 +8,22 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    let overpassManager = OverpassRequestManager()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        overpassManager.makeOverpassRequest(forBoundingBox: 17.4554726, 78.3709717, 17.4607127, 78.3764648) { result in
+            print(result)
+            print("Node Count: \(String(describing: result["nodeCount"]))")
+            print("Way Count: \(String(describing: result["wayCount"]))")
+            print("Relation Count: \(String(describing: result["relCount"]))")
+
+        }
+
     }
+    
 
 
 }
