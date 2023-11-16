784f9d217e0e2a742453b0909874b55629890421
Achyut Kumar M <achyutkumar.m@gmail.com>
2023-11-17 00:10:13 +0530

    remove overpassManager from ViewController

--- GoInfoGame/GoInfoGame/ViewController.swift
+++ GoInfoGame/GoInfoGame/ViewController.swift
@@ -14,18 +14,12 @@ class ViewController: UIViewController {
         vc.modalPresentationStyle = .fullScreen
         present(vc, animated: true)
     }
-    let overpassManager = OverpassRequestManager()
+   
 
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        overpassManager.makeOverpassRequest(forBoundingBox: 17.4554726, 78.3709717, 17.4607127, 78.3764648) { result in
-            print(result)
-            print("Node Count: \(String(describing: result["nodeCount"]))")
-            print("Way Count: \(String(describing: result["wayCount"]))")
-            print("Relation Count: \(String(describing: result["relCount"]))")
 
-        }
 
     }
     
