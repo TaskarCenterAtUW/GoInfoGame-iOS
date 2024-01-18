72af350773ce35de46cd6f9a9cd884a6d0b56264
Naresh Kumar D <susrisha@gmail.com>
2024-01-18 12:21:25 +0530

    Steps incline quest added
    Added quest for steps incline

---
+++ GoInfoGame/GoInfoGame/quests/StepsIncline/StepsIncline.swift
@@ -0,0 +1,44 @@
+//
+//  StepsIncline.swift
+//  GoInfoGame
+//
+//  Created by Naresh Devalapally on 1/18/24.
+//
+
+import Foundation
+import UIKit
+import SwiftUI
+
+class StepsIncline: Quest{
+    func onAnswer(answer: StepsInclineDirection) {
+         
+    }
+    
+    typealias AnswerClass = StepsInclineDirection
+    
+    var title: String = "StepsIncline"
+    
+    var filter: String = ""
+    
+    var icon: UIImage = #imageLiteral(resourceName: "steps.pdf")
+    
+    var wikiLink: String = ""
+    
+    var changesetComment: String = ""
+    
+    var form: AnyView = AnyView(StepsInclineForm()) // temporary
+    
+    var relationData: Any? = nil
+    
+    var displayUnit: DisplayUnit {
+        DisplayUnit(title: self.title, description: "",parent: self)
+    }
+    
+    
+}
+
+enum StepsInclineDirection {
+    case up
+    case down
+    
+}
