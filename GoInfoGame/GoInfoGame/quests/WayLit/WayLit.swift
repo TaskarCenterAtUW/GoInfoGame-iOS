b4f8e4d45c1b690956c25596c6d88506aecb207c
iamrajeshk <44539669+iamrajeshk@users.noreply.github.com>
2024-01-23 12:48:58 +0530

    Added filter query
    Added filters for BusStopLit, StepsIncline, StepsRamp, TactilePavingSteps, WayLit

--- GoInfoGame/GoInfoGame/quests/WayLit/WayLit.swift
+++ GoInfoGame/GoInfoGame/quests/WayLit/WayLit.swift
@@ -11,7 +11,29 @@ import SwiftUI
 
 class WayLit: Quest{
     var title: String = "Way Lit"
-    var filter: String = ""
+    var filter: String = """
+    ways with
+            (
+              highway ~ \(RoadTypes.litResidentialRoads.joined(separator: "|"))
+              or highway ~ \(RoadTypes.litNonResidentialRoads.joined(separator: "|")) and
+              (
+                sidewalk ~ both|left|right|yes|separate
+                or ~"${(MAXSPEED_TYPE_KEYS + "maxspeed").joinToString("|")}" ~ ".*:(urban|.*zone.*|nsl_restricted)"
+                or maxspeed <= 60
+              )
+              or highway ~ \(RoadTypes.litWays.joined(separator: "|"))
+              or highway = path and (foot = designated or bicycle = designated)
+            )
+            and
+            (
+              !lit
+              or lit = no and lit older today -8 years
+              or lit older today -16 years
+            )
+            and (access !~ private|no or (foot and foot !~ private|no))
+            and indoor != yes
+            and ~path|footway|cycleway !~ link
+    """
     var icon: UIImage = #imageLiteral(resourceName: "add_way_lit")
     var wikiLink: String = ""
     var changesetComment: String = ""
@@ -39,4 +61,15 @@ enum LitStatus {
 
 struct WayLitAnswer: WayLitOrIsStepsAnswer {
     var litStatus: LitStatus
+}
+
+struct RoadTypes {
+    static let litResidentialRoads = ["residential", "living_street", "pedestrian"]
+    
+    static let litNonResidentialRoads = [
+        "motorway", "motorway_link", "trunk", "trunk_link", "primary", "primary_link",
+        "secondary", "secondary_link", "tertiary", "tertiary_link", "unclassified", "service"
+    ]
+    
+    static let litWays = ["footway", "cycleway", "steps"]
 }
