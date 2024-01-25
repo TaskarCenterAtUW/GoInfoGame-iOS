bfb622729c3d4f5cab60d29ad35c6253c0b8ddf5
shweta2101 <shwethap@vindago.in>
2024-01-25 20:04:07 +0530

    Added bounding box on user's current location

--- GoInfoGame/GoInfoGame/UI/Map/LocationManagerCoordinator.swift
+++ GoInfoGame/GoInfoGame/UI/Map/LocationManagerCoordinator.swift
@@ -7,21 +7,49 @@
 
 import Foundation
 import CoreLocation
+import MapKit
 
-class LocationManagerCoordinator: NSObject, CLLocationManagerDelegate {
-    var parent: MapView?
-    init(parent: MapView) {
-        self.parent = parent
+final class LocationManagerCoordinator: NSObject, ObservableObject {
+    
+    private let locationManager = CLLocationManager()
+    @Published var region = MKCoordinateRegion(
+        center: CLLocationCoordinate2D(latitude: 47.6062, longitude: -122.3321),
+        span: MKCoordinateSpan(latitudeDelta: 0.05, longitudeDelta: 0.05))
+    var locationUpdateHandler: ((CLLocation) -> Void)?
+    override init() {
+        super.init()
+        self.locationManager.delegate = self
+        self.locationManager.desiredAccuracy = kCLLocationAccuracyBest
+        self.setup()
     }
+    
+    func setup() {
+        switch locationManager.authorizationStatus {
+        case .authorizedWhenInUse:
+            locationManager.requestLocation()
+        case .notDetermined:
+            locationManager.startUpdatingLocation()
+            locationManager.requestWhenInUseAuthorization()
+        default:
+            break
+        }
+    }
+}
+
+extension LocationManagerCoordinator: CLLocationManagerDelegate {
+    func locationManagerDidChangeAuthorization(_ manager: CLLocationManager) {
+        guard .authorizedWhenInUse == manager.authorizationStatus else { return }
+        locationManager.requestLocation()
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {
+        print("Something went wrong: \(error)")
+    }
+    
     func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        locationManager.stopUpdatingLocation()
         guard let location = locations.first else { return }
         print("Location received")
-        parent?.centerMapOnLocation(location: location)
-        // Try to load the quests
-       
-    }
-    func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {
-        print("Error requesting location: \(error.localizedDescription)")
-       
+        locationUpdateHandler?(location)
     }
 }
--- GoInfoGame/GoInfoGame/UI/Map/MapView.swift
+++ GoInfoGame/GoInfoGame/UI/Map/MapView.swift
@@ -7,7 +7,6 @@
 
 import SwiftUI
 import MapKit
-import UIKit
 import CoreLocation
 
 struct MapView: View {
@@ -18,84 +17,95 @@ struct MapView: View {
         center: CLLocationCoordinate2D(latitude: 47.6062, longitude: -122.3321),
         span: MKCoordinateSpan(latitudeDelta: 0.05, longitudeDelta: 0.05)
     )
-    let items: [ DisplayUnitWithCoordinate] = AppQuestManager.shared.fetchQuestsFromDB()
-    @State var selectedQuest: DisplayUnit?
-    var btnBack : some View { Button(action: {
-        if isMapFromOnboarding {
-            isMapFromOnboarding = false
-            NavigationUtil.popToRootView()
-        } else {
-            presentationMode.wrappedValue.dismiss()
+    let items: [DisplayUnitWithCoordinate] = AppQuestManager.shared.fetchQuestsFromDB()
+    @State private var selectedQuest: DisplayUnit?
+    @StateObject var manager = LocationManager()
+    @State private var currentLocation: CLLocation?
+    
+    var btnBack: some View {
+        Button(action: {
+            if isMapFromOnboarding {
+                isMapFromOnboarding = false
+                NavigationUtil.popToRootView()
+            } else {
+                presentationMode.wrappedValue.dismiss()
+            }
+        }) {
+            HStack(spacing: 0){
+                Image(systemName: "chevron.left")
+                    .scaleEffect(0.50)
+                    .font(Font.title.weight(.medium))
+                Text("Back")
+            }
         }
-    }) {
-        HStack(spacing: 0){
-            Image(systemName: "chevron.left")
-                .scaleEffect(0.50)
-                .font(Font.title.weight(.medium)
-                )
-            Text("Back")
-        }
-    }}
+    }
+    
     var body: some View {
-        Map(coordinateRegion: .constant(MKCoordinateRegion(center: CLLocationCoordinate2D(latitude: 47.6062, longitude: -122.3321), span: MKCoordinateSpan(latitudeDelta: 0.05, longitudeDelta: 0.05))), showsUserLocation: true, userTrackingMode: $userTrackingMode, annotationItems: items) { item in
-            MapAnnotation(coordinate: item.coordinateInfo) {
-                Button {
-                    selectedQuest = item.displayUnit
-                    print(selectedQuest as Any,"selectedQuest quest")
-                } label: {
-                    Image(uiImage: item.displayUnit.parent?.icon ?? UIImage(imageLiteralResourceName: "mapPoint"))
-                        .resizable()
-                        .aspectRatio(contentMode: .fit)
-                        .clipShape(Circle())
-                        .overlay(Circle().stroke(Color.white, lineWidth: 2))
-                        .shadow(radius: 10)
-                        .frame(width: 40, height: 40)
-                        .offset(y: -10)
+        VStack {
+            Map(coordinateRegion: $coordinateRegion, showsUserLocation: true, userTrackingMode: $userTrackingMode, annotationItems: items) { item in
+                MapAnnotation(coordinate: item.coordinateInfo) {
+                    Button {
+                        selectedQuest = item.displayUnit
+                        print(selectedQuest as Any,"selectedQuest quest")
+                    } label: {
+                        Image(uiImage: item.displayUnit.parent?.icon ?? UIImage(imageLiteralResourceName: "mapPoint"))
+                            .resizable()
+                            .aspectRatio(contentMode: .fit)
+                            .clipShape(Circle())
+                            .overlay(Circle().stroke(Color.white, lineWidth: 2))
+                            .shadow(radius: 10)
+                            .frame(width: 40, height: 40)
+                            .offset(y: -10)
+                    }
                 }
             }
-            
-        }.sheet(item: $selectedQuest) { selectedQuest in
+            Spacer()
+        }
+        .sheet(item: $selectedQuest) { selectedQuest in
             if #available(iOS 16.0, *) {
                 selectedQuest.parent?.form.presentationDetents(getSheetSize(sheetSize: selectedQuest.sheetSize ?? .MEDIUM))
             } else {
                 // Nothing here
             }
-            
         }
         .onAppear {
-//            centerMapOnDefaultLocation()
-        }.navigationBarBackButtonHidden(true)
-            .toolbar {
-                ToolbarItem(placement: .topBarLeading) {
-                    btnBack
-                }
+            manager.locationUpdateHandler = { location in
+                self.currentLocation =  CLLocation(latitude: location.latitude, longitude: location.longitude)
+//                centerMapOnLocation(location: location)
             }
+        }
+        .navigationBarBackButtonHidden(true)
+        .toolbar {
+            ToolbarItem(placement: .topBarLeading) {
+                btnBack
+            }
+        }
     }
-    @State private var locationManager = CLLocationManager()
+   
 }
-
 
 extension MapView {
-    private func centerMapOnDefaultLocation() {
-        let userLocation = CLLocationCoordinate2D(latitude: 47.6062, longitude: -122.3321)
+     func centerMapOnLocation(location: CLLocation) {
+        let userLocation = location.coordinate
         coordinateRegion.center = userLocation
-        coordinateRegion.span = MKCoordinateSpan(latitudeDelta: 0.02, longitudeDelta: 0.02)
+        let boundingBox = boundingBoxAroundLocation(location: location, distance: 1000)
+         let center = CLLocationCoordinate2D(latitude: (boundingBox.minLat + boundingBox.maxLat) / 2, longitude: (boundingBox.minLon + boundingBox.maxLon) / 2)
+             let span = MKCoordinateSpan(latitudeDelta: boundingBox.maxLat - boundingBox.minLat, longitudeDelta: boundingBox.maxLon - boundingBox.minLon)
+             coordinateRegion = MKCoordinateRegion(center: center, span: span)
+
+         var _: () = AppQuestManager.shared.fetchData(fromBBOx: boundingBox)
     }
-    func centerMapOnLocation(location: CLLocation) {
-        let region = MKCoordinateRegion(
-            center: location.coordinate,
-            span: MKCoordinateSpan(latitudeDelta: 0.05, longitudeDelta: 0.05)
-        )
-        coordinateRegion = region
-    }
-    private func requestLocation() {
-        let coordinator = LocationManagerCoordinator(parent: self)
-        locationManager.delegate = coordinator
-        locationManager.requestWhenInUseAuthorization()
-        locationManager.startUpdatingLocation()
+     func boundingBoxAroundLocation(location: CLLocation, distance: CLLocationDistance) -> BBox {
+        let latDelta = 0.008
+        let lonDelta = 0.008
+        let coordinate = location.coordinate
+        let minLat = coordinate.latitude - latDelta
+        let maxLat = coordinate.latitude + latDelta
+        let minLon = coordinate.longitude - lonDelta
+        let maxLon = coordinate.longitude + lonDelta
+        return BBox(minLat: minLat, maxLat: maxLat, minLon: minLon, maxLon: maxLon)
     }
 }
-
 #Preview {
     MapView()
 }
