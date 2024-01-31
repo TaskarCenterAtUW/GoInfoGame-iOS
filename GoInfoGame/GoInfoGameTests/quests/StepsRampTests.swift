a760a3aba80bd1c4e0110a2a8ed5ac359ca49182
Achyut Kumar M <achyutkumar.m@gmail.com>
2024-01-31 23:40:52 +0530

    test steps ramp query

--- GoInfoGame/GoInfoGameTests/quests/StepsRampTests.swift
+++ GoInfoGame/GoInfoGameTests/quests/StepsRampTests.swift
@@ -6,8 +6,15 @@
 //
 
 import XCTest
+@testable import GoInfoGame
+@testable import osmparser
 
 final class StepsRampTests: XCTestCase {
+    
+    let dbInstance = DatabaseConnector.shared
+    var nodes: [Node] = []
+    var ways: [Way] = []
+    let stepsRamp = StepsRamp()
 
     override func setUpWithError() throws {
         // Put setup code here. This method is called before the invocation of each test method in the class.
@@ -15,6 +22,40 @@ final class StepsRampTests: XCTestCase {
 
     override func tearDownWithError() throws {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+    
+    /** Testing the query
+     ways with highway = steps
+              and (!indoor or indoor = no)
+              and access !~ private|no
+              and (!conveying or conveying = no)
+              and ramp != separate
+              and (
+                !ramp
+                or (ramp = yes and !ramp:stroller and !ramp:bicycle and !ramp:wheelchair)
+                or ramp = no and ramp older today -4 years
+                or ramp older today -8 years
+              )
+     
+     */
+    
+    func testStepsRampQuery() throws {
+        assertIsNotApplicable(element: TestQuestUtils.node(tags: ["" : ""]))
+        assertIsNotApplicable(element: TestQuestUtils.way(tags: ["highway": "steps", "access": "private"]))
+        assertIsApplicable(element: TestQuestUtils.way(tags: ["highway": "steps", "conveying": "no"]))
+        assertIsNotApplicable(element: TestQuestUtils.way(tags: ["highway": "steps", "ramp": "seperate"]))
+        assertIsApplicable(element: TestQuestUtils.way(tags: ["highway" : "steps", "ramp": "yes", "ramp:date": "2023-01-31"]))
+        assertIsApplicable(element: TestQuestUtils.way(tags: ["highway" : "steps", "indoor": "no", "ramp:date": "2023-01-31"]))
+       
+
+    }
+    
+    private func assertIsApplicable(element: Element) {
+        XCTAssertTrue(stepsRamp.isApplicable(element: element))
+    }
+    
+    private func assertIsNotApplicable(element: Element) {
+        XCTAssertFalse(stepsRamp.isApplicable(element: element))
     }
 
     func testExample() throws {
