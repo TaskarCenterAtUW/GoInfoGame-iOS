1b0fd5c286ca870362d1b97f324cc23b81e32c8a
Achyut Kumar M <achyutkumar.m@gmail.com>
2024-02-01 03:57:03 +0530

    test busstop lit query

--- GoInfoGame/GoInfoGameTests/quests/BusStopLitTests.swift
+++ GoInfoGame/GoInfoGameTests/quests/BusStopLitTests.swift
@@ -6,8 +6,13 @@
 //
 
 import XCTest
+@testable import GoInfoGame
+@testable import osmparser
+
 
 final class BusStopLitTests: XCTestCase {
+    
+    let busStopLit = BusStopLit()
 
     override func setUpWithError() throws {
         // Put setup code here. This method is called before the invocation of each test method in the class.
@@ -15,6 +20,43 @@ final class BusStopLitTests: XCTestCase {
 
     override func tearDownWithError() throws {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+    
+    /** Testing the query
+     nodes, ways, relations with
+             (
+               public_transport = platform
+               or (highway = bus_stop and public_transport != stop_position)
+             )
+             and physically_present != no and naptan:BusStopType != HAR
+             and location !~ underground|indoor
+             and indoor != yes
+             and (!level or level >= 0)
+             and (
+               !lit
+               or lit = no and lit older today -8 years
+               or lit older today -16 years
+             )
+     
+     */
+    
+    func testBusStopLitQuery() throws {
+        assertIsNotApplicable(element: TestQuestUtils.way(tags: ["" : ""]))
+        assertIsNotApplicable(element: TestQuestUtils.rel(tags: ["public_transport" :"platform", "indoor" : "no" ]))
+        assertIsApplicable(element: TestQuestUtils.node(tags: ["public_transport": "platform"]))
+        assertIsNotApplicable(element: TestQuestUtils.node(tags: ["highway": "bus_stop", "public_transport": "stop_position", "physically_present": "yes", "naptan:BusStopType": "HAR", "location": "underground", "indoor": "no", "level": "1", "lit": "no"]))
+        assertIsNotApplicable(element: TestQuestUtils.node(tags: ["public_transport": "platform", "physically_present": "no"])) // physically_present is not allowed to be 'no'
+        assertIsNotApplicable(element: TestQuestUtils.node(tags: ["public_transport": "platform", "lit": "yes", "lit:date": "2015-01-31"])) // lit older than 8 years
+        assertIsApplicable(element: TestQuestUtils.node(tags: ["public_transport": "platform", "level": "0"]))
+    }
+    
+    
+    private func assertIsApplicable(element: Element) {
+        XCTAssertTrue(busStopLit.isApplicable(element: element))
+    }
+    
+    private func assertIsNotApplicable(element: Element) {
+        XCTAssertFalse(busStopLit.isApplicable(element: element))
     }
 
     func testExample() throws {
