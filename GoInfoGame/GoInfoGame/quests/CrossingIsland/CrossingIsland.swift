e12623fe4fb45797358c1037a57c0878dc74bb5b
shweta2101 <shwethap@vindago.in>
2024-02-08 15:58:40 +0530

    init commit

---
+++ GoInfoGame/GoInfoGame/quests/CrossingIsland/CrossingIsland.swift
@@ -0,0 +1,8 @@
+//
+//  CrossingIsland.swift
+//  GoInfoGame
+//
+//  Created by Lakshmi Shweta Pochiraju on 08/02/24.
+//
+
+import Foundation
---
+++ GoInfoGame/GoInfoGame/quests/CrossingIsland/CrossingIslandForm.swift
@@ -0,0 +1,18 @@
+//
+//  CrossingIslandForm.swift
+//  GoInfoGame
+//
+//  Created by Lakshmi Shweta Pochiraju on 08/02/24.
+//
+
+import SwiftUI
+
+struct CrossingForm: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CrossingForm()
+}
