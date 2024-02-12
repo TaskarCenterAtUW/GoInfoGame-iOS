b8a5928c67f884fb1f8c62619b8a43513ae13cde
Achyut Kumar M <achyutkumar.m@gmail.com>
2024-02-13 02:02:04 +0530

    add custom sure alert to way lit form

--- GoInfoGame/GoInfoGame/quests/WayLit/WayLitForm.swift
+++ GoInfoGame/GoInfoGame/quests/WayLit/WayLitForm.swift
@@ -11,16 +11,51 @@ import SwiftUI
 struct WayLitForm: View, QuestForm {
     var action: ((YesNoAnswer) -> Void)?
     typealias AnswerClass = YesNoAnswer
+    @State private var shouldShowAreYouSureAlert = false
+    @State private var answer: YesNoAnswer = .unknown
+    var action: ((AnswerClass) -> Void)?
+    
+    
+    typealias AnswerClass = WayLitOrIsStepsAnswer
+    
     
     var body: some View {
         VStack{
             QuestionHeader(icon: Image("add_way_lit"), title: LocalizedStrings.questLitTitle.localized, subtitle: "SideWalk")
             YesNoView(action: action)
              .background(
+            YesNoView { selectedAnswer in
+                self.answer = selectedAnswer
+                // Apply answer as needed
+                switch answer {
+                case .no :
+                    print("Not lit")
+                    // present are you sure alert
+                    self.shouldShowAreYouSureAlert = true
+                case .yes :
+                    print("Lit")
+                case .other:
+                    print("Present other options")
+                case .unknown:
+                    print("Dont know yet.")
+                }
+            } .background(
                 RoundedRectangle(cornerRadius: 10)
                     .fill(Color.white)
                     .shadow(color: .gray, radius: 2, x: 0, y: 2))
         }.padding()
+            .overlay(
+                Group {
+                    if shouldShowAreYouSureAlert {
+                        CustomSureAlert(onCancel: {
+                            self.shouldShowAreYouSureAlert = false
+                        }, onConfirm: {
+                            self.shouldShowAreYouSureAlert = false
+                        })
+
+                    }
+                }
+            )
     }
 }
 
