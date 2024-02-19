07c6e79544c49f57efc89e5ef66877480d983b0d
Achyut Kumar M <achyutkumar.m@gmail.com>
2024-02-19 14:58:29 +0530

    add YesNo to HandRailForm

--- GoInfoGame/GoInfoGame/quests/Handrail/HandRailForm.swift
+++ GoInfoGame/GoInfoGame/quests/Handrail/HandRailForm.swift
@@ -10,17 +10,36 @@ import SwiftUI
 struct HandRailForm: View, QuestForm {
     typealias AnswerClass = YesNoAnswer
     var action: ((YesNoAnswer) -> Void)?
+    @State private var isShowingAreYouSure = false
+    @State private var selectedAnswer: YesNoAnswer = .unknown
     
     var body: some View {
-        VStack{
-            QuestionHeader(icon: Image("steps_handrail"),
-                           title: "Do these steps have handrail?",
-                           subtitle: "")
-            YesNoView(action: action)
+        ZStack {
+            VStack{
+                QuestionHeader(icon: Image("steps_handrail"),
+                               title: "Do these steps have handrail?",
+                               subtitle: "")
+                YesNoView(action: { answer in
+                    self.selectedAnswer = answer
+                    if answer == .yes || answer == .no {
+                        self.isShowingAreYouSure.toggle()
+                    }
+                })
                 .background(RoundedRectangle(cornerRadius: 10)
                     .fill(Color.white)
                     .shadow(color: .gray, radius: 2, x: 0, y: 2))
-        }.padding()
+            }.padding()
+            if isShowingAreYouSure {
+                CustomSureAlert(onCancel: {
+                    self.isShowingAreYouSure = false
+                }, onConfirm: {
+                    self.isShowingAreYouSure = false
+                    self.action?(selectedAnswer)
+                    print("Answer is: \(selectedAnswer)")
+                })
+                .zIndex(1)
+            }
+        }
     }
 }
 
