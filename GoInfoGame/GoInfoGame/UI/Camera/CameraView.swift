6d34f560e917b2af4b2f00be1137a58822790e10
Achyut Kumar M <achyutkumar.m@gmail.com>
2024-02-23 10:55:06 +0530

    add crosshair to use as reference to start measuring

--- GoInfoGame/GoInfoGame/UI/Camera/CameraView.swift
+++ GoInfoGame/GoInfoGame/UI/Camera/CameraView.swift
@@ -8,57 +8,6 @@
 import SwiftUI
 import ARKit
 
-struct MeasuringView: View {
-    
-    @Binding var measuring: Bool
-    @State private var startValue = SCNVector3()
-    @State private var endValue = SCNVector3()
-    
-    var body: some View {
-        VStack {
-            ARViewContainer(measuring: $measuring, startValue: $startValue, endValue: $endValue)
-            Button(action: {
-                self.toggleMeasuring()
-            }) {
-                Text("+")
-                    .font(.system(size: 10))
-                    .padding()
-                    .foregroundColor(.white)
-                    .background(Color.blue)
-                    .cornerRadius(10)
-            }
-            Text("Result: \(measurementResult)")
-            
-        }
-    }
-    
-    var measurementResult: String {
-        let cm = startValue.distance(from: endValue) * 100.0
-        let inch = cm * 0.3937007874
-        return String(format: "%.2f cm / %.2f\"", cm, inch)
-    }
-    
-    func toggleMeasuring() {
-        measuring.toggle()
-        if !measuring {
-            resetValues()
-        }
-    }
-    
-    func resetValues() {
-        startValue = SCNVector3()
-        endValue = SCNVector3()
-    }
-    
-    func makeCoordinator() -> Coordinator {
-        Coordinator()
-    }
-    
-    class Coordinator: NSObject, ARSCNViewDelegate {
-        
-    }
-}
-
 struct ARMeasuringView: View {
     @Binding var measuring: Bool
     @State private var startValue = SCNVector3()
@@ -114,6 +63,11 @@ struct ARViewContainer: UIViewRepresentable {
         let configuration = ARWorldTrackingConfiguration()
         arView.session.run(configuration)
         
+        let crosshairNode = createCrosshairNode(size: arView.bounds.size)
+        let skScene = SKScene(size: arView.bounds.size)
+        skScene.addChild(crosshairNode)
+        arView.overlaySKScene = skScene
+        
         return arView
     }
     
@@ -166,5 +120,16 @@ struct ARViewContainer: UIViewRepresentable {
         let line = SCNGeometry.line(from: vertices)
         let lineNode = SCNNode(geometry: line)
         return lineNode
+    }
+    
+    func createCrosshairNode(size: CGSize) -> SKNode {
+        let crosshairLabel = SKLabelNode(text: "+")
+        crosshairLabel.fontSize = 50
+        crosshairLabel.fontColor = .white
+        crosshairLabel.position = CGPoint(x: size.width / 2, y: size.height / 2)
+        
+        let crosshairNode = SKNode()
+        crosshairNode.addChild(crosshairLabel)
+        return crosshairNode
     }
 }
