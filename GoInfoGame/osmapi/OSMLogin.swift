2cb5ba41fb4b0de9e74a8581fc8bd9c3c32ae261
Naresh Kumar D <susrisha@gmail.com>
2024-02-26 12:28:59 +0530

    Update OSMLogin.swift
    Added documentation for OSMLogin

--- GoInfoGame/osmapi/OSMLogin.swift
+++ GoInfoGame/osmapi/OSMLogin.swift
@@ -6,11 +6,15 @@
 //
 
 import Foundation
+/// Structure to store the user credentials
 public struct OSMLogin {
+    /// user name
     let username: String
+    /// the password
     let password: String
     
-    // Header information needed
+    /// Fetches the header information to be added
+    /// - returns String
     func getHeaderData() -> String {
         let loginString = "\(username):\(password)"
         let loginData = loginString.data(using: String.Encoding.utf8)!
@@ -18,14 +22,16 @@ public struct OSMLogin {
         return base64LoginString
     }
     
+    /// Production credentials
     public static var production : OSMLogin {
          OSMLogin(username: "nerope1097@wentcity.com", password: "$$WentCityErwin")
     }
     
+    /// Test server credentials
     public static var test : OSMLogin {
         OSMLogin(username: "nareshd@vindago.in", password: "a$hwa7hamA") // Need to change
     }
-    
+    /// Dev server credentials
     public static var testOSM : OSMLogin {
         OSMLogin(username: "giyiw35819@tupanda.com", password: "d.Dgq6J6aBy.dCJ") 
     }
