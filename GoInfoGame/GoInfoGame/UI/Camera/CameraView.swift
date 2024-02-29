7549139f826ac8bd684ca8b72727a4376abd95be
Achyut Kumar M <achyutkumar.m@gmail.com>
2024-03-01 00:29:38 +0530

    refactor MeasureWidthView to correct UI

--- GoInfoGame/GoInfoGame/UI/Camera/CameraView.swift
+++ GoInfoGame/GoInfoGame/UI/Camera/CameraView.swift
@@ -8,111 +8,139 @@
 import SwiftUI
 import ARKit
 
-struct ARMeasuringView: View {
-    @Binding var measuring: Bool
-    @State private var startValue = SCNVector3()
-    @State private var endValue = SCNVector3()
+struct MeasureWidthView: UIViewRepresentable {
     
-    var body: some View {
-        VStack {
-            ARViewContainer(measuring: $measuring, startValue: $startValue, endValue: $endValue)
-            Button(action: {
-                self.toggleMeasuring()
-            }) {
-                Text(measuring ? "Stop Measuring" : "Start Measuring")
-                    .padding()
-                    .foregroundColor(.white)
-                    .background(Color.blue)
-                    .cornerRadius(10)
-            }
-            
-            Text("Result: \(measurementResult)")
-        }
-    }
-    var measurementResult: String {
-        let cm = startValue.distance(from: endValue) * 100.0
-        let inch = cm * 0.3937007874
-        return String(format: "%.2f cm / %.2f\"", cm, inch)
-    }
+    @Binding var aimLabelHidden: Bool
+    @Binding var notReadyLabelHidden: Bool
+    @Binding var startMeasuring: Bool
+    @Binding var resultLabelText: String
     
-    func toggleMeasuring() {
-        measuring.toggle()
-        if !measuring {
-            resetValues()
-        }
-    }
+    let sceneView = ARSCNView()
+    let session = ARSession()
+    let sessionConfig = ARWorldTrackingConfiguration()
     
-    func resetValues() {
-        startValue = SCNVector3()
-        endValue = SCNVector3()
-    }
+    let vectorZero = SCNVector3()
+    var startValue = SCNVector3()
+    var endValue = SCNVector3()
+    
+    var startPointNode: SCNNode?
+    var endPointNode: SCNNode?
+    var lineNode: SCNNode?
+    
+    @State private var isMeasuring = false
 
-}
-
-struct ARViewContainer: UIViewRepresentable {
-    @Binding var measuring: Bool
-    @Binding var startValue: SCNVector3
-    @Binding var endValue: SCNVector3
     
-    func makeUIView(context: Context) -> ARSCNView {
-        let arView = ARSCNView()
-        arView.delegate = context.coordinator
-        let scene = SCNScene()
-        arView.scene = scene
-        
-        let configuration = ARWorldTrackingConfiguration()
-        arView.session.run(configuration)
-        
-        let crosshairNode = createCrosshairNode(size: arView.bounds.size)
-        let skScene = SKScene(size: arView.bounds.size)
-        skScene.addChild(crosshairNode)
-        arView.overlaySKScene = skScene
-        
-        return arView
+     func makeUIView(context: Context) -> ARSCNView {
+        sceneView.delegate = context.coordinator
+        sceneView.session = session
+        session.run(sessionConfig, options: [.resetTracking, .removeExistingAnchors])
+       
+        return sceneView
     }
+        
     
     func updateUIView(_ uiView: ARSCNView, context: Context) {
-
+        if startMeasuring {
+            context.coordinator.startMeasuring()
+        } else {
+            if isMeasuring {
+                context.coordinator.stopMeasuring()
+                isMeasuring = false
+            }
+        }
     }
     
     func makeCoordinator() -> Coordinator {
         Coordinator(self)
     }
     
+    
     class Coordinator: NSObject, ARSCNViewDelegate {
-        var parent: ARViewContainer
+        var parent: MeasureWidthView
         
-        init(_ parent: ARViewContainer) {
+        init(_ parent: MeasureWidthView) {
             self.parent = parent
         }
         
+        @objc func measuringChanged() {
+            if parent.startMeasuring {
+                startMeasuring()
+            } else {
+                stopMeasuring()
+            }
+        }
+        
+        func startMeasuring() {
+            parent.resetValues()
+            parent.startMeasuring = true
+            
+        }
+        
+        func stopMeasuring() {
+            parent.startMeasuring = false
+            parent.endPointNode = parent.createPointNode(at: parent.endValue, color: .red)
+            parent.sceneView.scene.rootNode.addChildNode(parent.endPointNode!)
+            parent.updateResultLabel(parent.startValue.distance(from: parent.endValue))
+        }
+        
+        
         func renderer(_ renderer: SCNSceneRenderer, updateAtTime time: TimeInterval) {
             DispatchQueue.main.async {
-                if self.parent.measuring {
-                    self.parent.detectObjects(in: renderer as! ARSCNView)
-                }
+                self.parent.detectObjects()
             }
         }
     }
     
-    func detectObjects(in arView: ARSCNView) {
-        guard let worldPos = arView.realWorldVector(screenPos: arView.center) else { return }
-        
-        if startValue == SCNVector3() {
-            startValue = worldPos
-        } else {
-            endValue = worldPos
+    mutating func detectObjects() {
+        if let worldPos = sceneView.realWorldVector(screenPos: sceneView.center) {
+            aimLabelHidden = false
+            notReadyLabelHidden = true
+            if startMeasuring {
+                if startValue == vectorZero {
+                    startValue = worldPos
+                    startPointNode = createPointNode(at: worldPos, color: .green)
+                    sceneView.scene.rootNode.addChildNode(startPointNode!)
+                    isMeasuring = true
+                }
+                
+                endValue = worldPos
+                
+                if let line = lineNode {
+                    line.removeFromParentNode()
+                }
+                lineNode = createLineNode(from: startValue, to: endValue)
+                sceneView.scene.rootNode.addChildNode(lineNode!)
+            }
         }
-        
-        updateLine(in: arView)
     }
     
-    func updateLine(in arView: ARSCNView) {
-        let lineNode = createLineNode(from: startValue, to: endValue)
-        arView.scene.rootNode.enumerateChildNodes { (node, _) in
-            node.removeFromParentNode()
+    mutating func resetValues() {
+        startMeasuring = false
+        startValue = SCNVector3()
+        endValue = SCNVector3()
+        
+        startPointNode?.removeFromParentNode()
+        endPointNode?.removeFromParentNode()
+        lineNode?.removeFromParentNode()
+        
+        updateResultLabel(0.0)
+        
+    }
+    
+    func updateResultLabel(_ value: Float) {
+        let cm = value * 100.0
+        let inch = cm*0.3937007874
+        DispatchQueue.main.async {
+            self.resultLabelText = String(format: "%.2f cm / %.2f\"", cm, inch)
         }
-        arView.scene.rootNode.addChildNode(lineNode)
+    }
+    
+    func createPointNode(at position: SCNVector3, color: UIColor) -> SCNNode {
+        let sphere = SCNSphere(radius: 0.005)
+        sphere.firstMaterial?.diffuse.contents = color
+        let node = SCNNode(geometry: sphere)
+        node.position = position
+        return node
     }
     
     func createLineNode(from startPoint: SCNVector3, to endPoint: SCNVector3) -> SCNNode {
@@ -120,16 +148,5 @@ struct ARViewContainer: UIViewRepresentable {
         let line = SCNGeometry.line(from: vertices)
         let lineNode = SCNNode(geometry: line)
         return lineNode
-    }
-    
-    func createCrosshairNode(size: CGSize) -> SKNode {
-        let crosshairLabel = SKLabelNode(text: "+")
-        crosshairLabel.fontSize = 50
-        crosshairLabel.fontColor = .white
-        crosshairLabel.position = CGPoint(x: size.width / 2, y: size.height / 2)
-        
-        let crosshairNode = SKNode()
-        crosshairNode.addChild(crosshairLabel)
-        return crosshairNode
     }
 }
--- GoInfoGame/GoInfoGame/UI/Camera/MeasureSidewalkView.swift
+++ GoInfoGame/GoInfoGame/UI/Camera/MeasureSidewalkView.swift
@@ -9,13 +9,50 @@ import SwiftUI
 
 struct MeasureSidewalkView: View {
     
-    @State private var measuring = false
+    @State private var aimLabelHidden = true
+    @State private var notReadyLabelHidden = false
+    @State private var startMeasuring = false
+    @State private var resultLabelText = ""
     
     var body: some View {
-        VStack {
-            ARMeasuringView(measuring: $measuring)
-            
+        ZStack {
+           Spacer()
+            MeasureWidthView(aimLabelHidden: $aimLabelHidden, notReadyLabelHidden: $notReadyLabelHidden, startMeasuring: $startMeasuring, resultLabelText: $resultLabelText)
+            VStack {
+                Spacer()
+                Text(".")
+                    .padding()
+                    .font(.largeTitle)
+                
+                Spacer()
+                Button {
+                    toggleMeasuring()
+                } label: {
+                    Text("Measure")
+                        .fontWeight(.bold)
+                        .padding()
+                        .foregroundColor(.white)
+                        .background(Color.blue)
+                        .cornerRadius(8)
+                        .overlay(
+                            RoundedRectangle(cornerRadius: 8)
+                                .stroke(Color.blue, lineWidth: 2)
+                        )
+                }
+                
+                Spacer()
+            }
         }
+        if !resultLabelText.isEmpty {
+            VStack {
+                Text("Width is: \(resultLabelText)").padding()
+            }
+        }
+            
+    }
+    
+    func toggleMeasuring() {
+        startMeasuring.toggle()
     }
 }
 
