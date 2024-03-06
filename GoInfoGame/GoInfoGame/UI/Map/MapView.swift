998e53cf24c064e28fae707b9ad3ee1ce3e13ab7
Achyut Kumar M <achyutkumar.m@gmail.com>
2024-03-06 23:43:50 +0530

    add inferDirection and calculateDistance methods

--- GoInfoGame/GoInfoGame/UI/Map/MapView.swift
+++ GoInfoGame/GoInfoGame/UI/Map/MapView.swift
@@ -30,6 +30,19 @@ struct MapView: View {
                         viewModel.selectedQuest = item.displayUnit
                         isPresented = true
                         print(viewModel.selectedQuest as Any, "selectedQuest quest")
+                        let distance = calculateDistance(selectedAnnotation: item.coordinateInfo)
+                        print("Distance from the user is \(distance) meters" )
+                        
+                        let direction = inferDirection(selectedAnnotation: item.coordinateInfo)
+                        print("Direction is \(direction)")
+                    
+                        inferAddress { address in
+                            if let address = address {
+                                print("This waylit is on \(address) at \(distance) meters away on \(direction)")
+                            } else {
+                                
+                            }
+                        }
                     } label: {
                         Image(uiImage: item.displayUnit.parent?.icon ?? UIImage(imageLiteralResourceName: "mapPoint"))
                             .resizable()
@@ -61,6 +74,78 @@ struct MapView: View {
             isPresented = false
         }
     }
+    
+    // calculate distance between user current location and selected annotation
+    func calculateDistance(selectedAnnotation: CLLocationCoordinate2D) -> CLLocationDistance {
+        let userCurrentLocation = locationManagerDelegate.locationManager.location!.coordinate
+        let fromLocation = CLLocation(latitude: userCurrentLocation.latitude, longitude: userCurrentLocation.longitude)
+        let toLocation = CLLocation(latitude: selectedAnnotation.latitude, longitude: selectedAnnotation.longitude)
+        return CLLocationDistance(Int(fromLocation.distance(from: toLocation)))
+    }
+    
+    // infer direction
+    func inferDirection(selectedAnnotation: CLLocationCoordinate2D) -> String {
+        let userCurrentLocation = locationManagerDelegate.locationManager.location!.coordinate
+        let userLocationPoint = MKMapPoint(userCurrentLocation)
+        let destinationPoint = MKMapPoint(selectedAnnotation)
+        let angleRadians = atan2(destinationPoint.y - userLocationPoint.y, destinationPoint.x - userLocationPoint.x)
+        var angleDegrees = angleRadians * 180 / .pi
+        angleDegrees += 90 // Adjust to be relative to north
+        
+        if angleDegrees < 0 {
+            angleDegrees += 360
+        } else if angleDegrees >= 360 {
+            angleDegrees -= 360
+        }
+        
+        angleDegrees = (angleDegrees * 10).rounded() / 10
+        
+        var direction = ""
+        
+        print("Angle for inferDirection (degrees): \(angleDegrees)")
+
+        if angleDegrees >= 337.5 || angleDegrees < 22.5 {
+            direction = "north"
+        } else if angleDegrees >= 22.5 && angleDegrees < 67.5 {
+            direction = "northeast"
+        } else if angleDegrees >= 67.5 && angleDegrees < 112.5 {
+            direction = "east"
+        } else if angleDegrees >= 112.5 && angleDegrees < 157.5 {
+            direction = "southeast"
+        } else if angleDegrees >= 157.5 && angleDegrees < 202.5 {
+            direction = "south"
+        } else if angleDegrees >= 202.5 && angleDegrees < 247.5 {
+            direction = "southwest"
+        } else if angleDegrees >= 247.5 && angleDegrees < 292.5 {
+            direction = "west"
+        } else {
+            direction = "northwest"
+        }
+        return direction
+    }
+    
+    // infer address from user location coordinates
+    func inferAddress(completion: @escaping (String?) -> Void) {
+        let userCurrentLocation = locationManagerDelegate.locationManager.location!
+        let geocoder = CLGeocoder()
+        
+        geocoder.reverseGeocodeLocation(userCurrentLocation) { placemarks, error in
+            guard let placemark = placemarks?.first else {
+                completion(nil)
+                return
+            }
+            var addressComponents: [String] = []
+            if let streetNumber = placemark.subThoroughfare {
+                addressComponents.append(streetNumber)
+            }
+            if let streetName = placemark.thoroughfare {
+                addressComponents.append(streetName)
+            }
+            
+            let address = addressComponents.joined(separator: ", ")
+            completion(address)
+        }
+    }
 }
 
 #Preview {
@@ -72,4 +157,31 @@ public class MapViewPublisher: ObservableObject {
     public let dismissSheet = PassthroughSubject<Bool, Never>()
     static let shared = MapViewPublisher()
     private init() {}
+}
+
+
+extension CLPlacemark {
+
+    var address: String? {
+        if let name = name {
+            var result = name
+
+            if let street = thoroughfare, !street.isEmpty {
+                result += ", \(street)"
+            }
+
+            if let city = locality {
+                result += ", \(city)"
+            }
+
+            if let country = country {
+                result += ", \(country)"
+            }
+
+            return result
+        }
+
+        return nil
+    }
+
 }
