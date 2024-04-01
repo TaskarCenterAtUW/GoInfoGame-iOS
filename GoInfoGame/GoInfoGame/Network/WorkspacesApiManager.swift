e6b9f05c49130054cd73f6a39dbd5351b3d79166
Naresh Kumar D <susrisha@gmail.com>
2024-04-01 12:46:56 +0530

    Added API
    Added api for fetching workspaces

---
+++ GoInfoGame/GoInfoGame/Network/WorkspacesApiManager.swift
@@ -0,0 +1,44 @@
+//
+//  WorkspacesApiManager.swift
+//  GoInfoGame
+//
+//  Created by Naresh Devalapally on 4/1/24.
+//
+
+import Foundation
+// Singleton object that deals with workspaces APIs
+
+class WorkspacesApiManager {
+    
+    static let shared = WorkspacesApiManager()
+    private let listingURL = "https://www.jsonkeeper.com/b/Q80Q"
+    private init() {}
+    
+    func fetchWorkspaces(_ completion:@escaping (Result<WorkSpacesResponse, Error>)-> Void) {
+        
+        var request = URLRequest(url: URL(string: self.listingURL)!,timeoutInterval: Double.infinity)
+       
+        request.httpMethod = "GET"
+
+        let task = URLSession.shared.dataTask(with: request) { data, response, error in
+          guard let data = data else {
+            print(String(describing: error))
+            return
+          }
+        let theDecoder = JSONDecoder()
+        theDecoder.dateDecodingStrategy = .iso8601
+            do {
+                let decodedData = try theDecoder.decode(WorkSpacesResponse.self, from: data)
+                completion(.success(decodedData))
+            } catch  {
+                completion(.failure(error))
+            }
+          
+        }
+
+        task.resume()
+
+
+    }
+    
+}
---
+++ GoInfoGame/GoInfoGameTests/WorkspacesApiManagerTests.swift
@@ -0,0 +1,54 @@
+//
+//  WorkspacesApiManagerTests.swift
+//  GoInfoGameTests
+//
+//  Created by Naresh Devalapally on 4/1/24.
+//
+
+@testable import GoInfoGame
+
+import XCTest
+
+final class WorkspacesApiManagerTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        let workspacesAPI = WorkspacesApiManager.shared
+        let expec = expectation(description: "Fetches the workspaces around user")
+        
+        
+        workspacesAPI.fetchWorkspaces(){result in
+            switch result {
+            case .success(let workspacesResponse):
+                print(workspacesResponse)
+                print(workspacesResponse.workspaces)
+                
+            case .failure(let error):
+                print(error)
+                
+            }
+            expec.fulfill()
+        }
+        waitForExpectations(timeout: 15)
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+        // Any test you write for XCTest can be annotated as throws and async.
+        // Mark your test throws to produce an unexpected failure when your test encounters an uncaught error.
+        // Mark your test async to allow awaiting for asynchronous code to complete. Check the results with assertions afterwards.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
