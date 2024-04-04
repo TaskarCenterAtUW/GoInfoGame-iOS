08050526f18b65d2ced418b37e7ec199d07bcc13
shweta2101 <shwethap@vindago.in>
2024-04-04 11:23:06 +0530

    fixed caps text

--- GoInfoGame/GoInfoGame/UI/InitialView/InitialView.swift
+++ GoInfoGame/GoInfoGame/UI/InitialView/InitialView.swift
@@ -58,7 +58,7 @@ struct WorkspacesListView: View {
                         Text(workspace.name)
                     }
                 }
-            }
+            }.textCase(nil)
         }
         .navigationBarHidden(true)
     }
