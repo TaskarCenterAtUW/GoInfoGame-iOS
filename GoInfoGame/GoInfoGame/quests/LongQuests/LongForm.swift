94e3af3da3714c936e6f86a42cb9a7d7053ff3ed
Achyut Kumar M <achyutm@gaussiansolutions.com>
2024-08-05 14:35:08 +0530

    long form UI

--- GoInfoGame/GoInfoGame/quests/LongQuests/LongForm.swift
+++ GoInfoGame/GoInfoGame/quests/LongQuests/LongForm.swift
@@ -7,101 +7,40 @@
 
 import SwiftUI
 
+
 struct LongForm: View {
-    let form = dummyFormData
-    @State private var selectedImage : [String] = []
-    @Binding var feet: Int
-    @Binding var inches: Int
-    @Binding var isConfirmAlert: Bool
-
-    
-    let imagesFromSurfaces: [ImageData] = SELECTABLE_WAY_SURFACES.compactMap { surfaceString in
-        if let surface = Surface(rawValue: surfaceString) {
-            return ImageData(
-                id: surfaceString,
-                type: surfaceString,
-                imageName: surface.iconResId,
-                tag: surface.rawValue,
-                optionName:  surface.titleResId
-            )
-        } else {
-            print("Invalid Surface: \(surfaceString)")
-            return nil
-        }
-    }
-
+    @ObservedObject private var viewModel = LongFormViewModel()
+            
     var body: some View {
         List {
-            ForEach(dummyFormData.quests) { quest in
-                    
-                    viewWithoutChild(quest: quest)
-                    
-
+            ForEach(viewModel.longForm!.quests, id: \.questID) { quest in
+                if viewModel.shouldShowQuest(quest) {
+                    LongQuestView(quest: quest, onChoiceSelected: { selectedAnswerChoice in
+                        viewModel.updateAnswers(quest: quest, selectedAnswerChoice: selectedAnswerChoice)
+                    })
                 }
-            
-        }
-        .listStyle(GroupedListStyle())
-    }
-    
-    func viewWithChild(quest: LongQuest) -> some View {
-        
-        let imagesFromQuest: [LongImageData] = quest.questAnswerChoices.compactMap { choice in
-                        LongImageData(
-                            type: choice.value, imageName: choice.imageURL ?? "surface_asphalt",
-                            tag: choice.value, optionName: choice.choiceText
-                        )
-                    }
-        
-      return VStack(alignment: .leading) {
-            Text(quest.questTitle)
-                .font(.headline)
-            Text(quest.questDescription)
-                .font(.subheadline)
-            
-          if quest.questType.rawValue == "ExclusiveChoice" {
-              LongImageGridItemView(gridCount: 3, isLabelBelow: true, imageData: imagesFromQuest, isImageRotated: false, isDisplayImageOnly: false, isScrollable: true, allowMultipleSelection: quest.questType.rawValue == "MultiplesChoice" ? true : false, onTap: { (selectedImage) in
-                    print("Selected long form image is \(selectedImage)")
-                }, selectedImages: $selectedImage).frame(height: 350)
-
-          } else if quest.questType.rawValue == "Numeric" {
-                LongFormWidthView(feet: $feet, inches: $inches, isConfirmAlert: $isConfirmAlert)
             }
-        }
-        .padding(.vertical, 5)
             
-        
-        
-    }
-    
-    func viewWithoutChild(quest: LongQuest) -> some View {
-        let imagesFromQuest: [LongImageData] = quest.questAnswerChoices.compactMap { choice in
-                        LongImageData(
-                            type: choice.value, imageName: choice.imageURL ?? "surface_asphalt",
-                            tag: choice.value, optionName: choice.choiceText
-                        )
-                    }
-        
-      return VStack(alignment: .leading) {
-            Text(quest.questTitle)
-                .font(.headline)
-            Text(quest.questDescription)
-                .font(.subheadline)
-            
-          if quest.questType.rawValue == "ExclusiveChoice" {
-              LongImageGridItemView(gridCount: 3, isLabelBelow: true, imageData: imagesFromQuest, isImageRotated: false, isDisplayImageOnly: false, isScrollable: true, allowMultipleSelection: quest.questType.rawValue == "MultiplesChoice" ? true : false, onTap: { (selectedImage) in
-                    print("Selected long form image is \(selectedImage)")
-                }, selectedImages: $selectedImage).frame(height: 350)
-
-          } else if quest.questType.rawValue == "Numeric" {
-                LongFormWidthView(feet: $feet, inches: $inches, isConfirmAlert: $isConfirmAlert)
-
+            VStack {
+                Button(action: {
+                    print("Submit pressed")
+                    print(viewModel.answersToBeSubmitted)
+                }) {
+                    Text("Submit")
+                        .font(.custom("Lato-Bold", size: 16))
+                        .foregroundColor(.white)
+                        .padding()
+                        .frame(width: 200, height: 40)
+                        .background(Color(red: 135/255, green: 62/255, blue: 242/255))
+                        .cornerRadius(20)
+                }
+                .frame(maxWidth: .infinity)
             }
+            .frame(maxWidth: .infinity)
         }
-        .padding(.vertical, 5)
     }
 }
 
 #Preview {
-
-    LongForm(feet: .constant(10), inches: .constant(10), isConfirmAlert: .constant(false))
+    LongForm()
 }
