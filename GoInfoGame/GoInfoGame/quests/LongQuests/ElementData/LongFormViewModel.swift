6643fa7a1e19925b3ea1ecb6d8cd40ac7dd91cd4
Achyut Kumar M <achyutm@gaussiansolutions.com>
2024-08-05 14:35:57 +0530

    add LongFormViewModel and logic to update answers for submission

--- GoInfoGame/GoInfoGame/quests/LongQuests/ElementData/LongFormViewModel.swift
+++ GoInfoGame/GoInfoGame/quests/LongQuests/ElementData/LongFormViewModel.swift
@@ -6,3 +6,37 @@
 //
 
 import Foundation
+
+class LongFormViewModel: ObservableObject {
+    @Published var longForm: LongFormModel?
+    @Published var answers: [Int: String] = [:]
+    @Published var answersToBeSubmitted: [String: String] = [:]
+
+    
+    init() {
+        self.longForm = dummyFormData
+    }
+        
+     func shouldShowQuest(_ quest: LongQuest) -> Bool {
+        guard let dependency = quest.questAnswerDependency else {
+            return true
+        }
+        if let answeredValue = answers[dependency.questionID] {
+            return dependency.requiredValue.contains(answeredValue)
+        }
+        return false
+    }
+    
+    func updateAnswers(quest: LongQuest, selectedAnswerChoice: QuestAnswerChoice) {
+        answers[quest.questID] = selectedAnswerChoice.value
+        answersToBeSubmitted[quest.questTag] = selectedAnswerChoice.value
+        answersToBeRemoved()
+    }
+    
+    func answersToBeRemoved() {
+        guard let quests = longForm?.quests else { return }
+        let visibleQuestTags = quests.filter { shouldShowQuest($0)}.map { $0.questTag }
+        answersToBeSubmitted = answersToBeSubmitted.filter{ visibleQuestTags.contains($0.key) }
+        
+    }
+}
