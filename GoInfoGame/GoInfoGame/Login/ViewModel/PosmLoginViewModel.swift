c353fad413ba19560bdc031057b3802bb047bfda
Achyut Kumar M <achyutm@gaussiansolutions.com>
2024-08-15 04:02:13 +0530

    api integration

--- GoInfoGame/GoInfoGame/Login/ViewModel/PosmLoginViewModel.swift
+++ GoInfoGame/GoInfoGame/Login/ViewModel/PosmLoginViewModel.swift
@@ -14,6 +14,24 @@ class PosmLoginViewModel: ObservableObject {
     @Published var errorMessage: String?
     
     func performLogin() {
-        
+        ApiManager.shared.login(username: username, password: password) { result in
+            switch result {
+            case .success(let posmLoginSuccessResponse):
+                let accessToken = posmLoginSuccessResponse.accessToken
+                DispatchQueue.main.async {
+                    _ = KeychainManager.save(key: "accessToken", data: accessToken)
+                    self.isLoggedIn = true
+                }
+              
+            case .failure(let posmLoginErrorResponse):
+                let error = posmLoginErrorResponse.errors.first ?? "An error occured"
+                DispatchQueue.main.async {
+                    self.errorMessage = error
+                }
+             
+            case .error(let string):
+                self.errorMessage = string
+            }
+        }
     }
 }
