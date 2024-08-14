ead50e7ba0930e25ecf1a53648d6c226859d056e
Achyut Kumar M <achyutm@gaussiansolutions.com>
2024-08-14 23:30:46 +0530

    - add ElementType enum - change quests to dynamic based on elementType

--- GoInfoGame/GoInfoGame/quests/LongQuests/View/LongForm.swift
+++ GoInfoGame/GoInfoGame/quests/LongQuests/View/LongForm.swift
@@ -11,40 +11,75 @@ import SwiftUI
 struct LongForm: View, QuestForm {
     @ObservedObject private var viewModel = LongFormViewModel()
     
+    var elementType: LongFormElementType?
+    
     var action: (([String:String]) -> Void)?
     
     typealias AnswerClass = [String:String]
+    
+    @Environment(\.presentationMode) var presentationMode
             
     var body: some View {
-        List {
-            ForEach(viewModel.longForm!.quests, id: \.questID) { quest in
-                if viewModel.shouldShowQuest(quest) {
-                    LongQuestView(quest: quest, onChoiceSelected: { selectedAnswerChoice in
-                        viewModel.updateAnswers(quest: quest, selectedAnswerChoice: selectedAnswerChoice)
-                    })
+        VStack(alignment: .leading) {
+            DismissButtonView {
+                withAnimation {
+                    presentationMode.wrappedValue.dismiss()
                 }
             }
-            
-            VStack {
-                Button(action: {
-                    print("Submit pressed")
-                    print(viewModel.answersToBeSubmitted)
-                }) {
-                    Text("Submit")
-                        .font(.custom("Lato-Bold", size: 16))
-                        .foregroundColor(.white)
-                        .padding()
-                        .frame(width: 200, height: 40)
-                        .background(Color(red: 135/255, green: 62/255, blue: 242/255))
-                        .cornerRadius(20)
+            List {
+                if let quests = questsForLongForm() {
+                    ForEach(quests, id: \.questID) { quest in
+                        if viewModel.shouldShowQuest(quest) {
+                            LongQuestView(quest: quest, onChoiceSelected: { selectedAnswerChoice in
+                                viewModel.updateAnswers(quest: quest, selectedAnswerChoice: selectedAnswerChoice)
+                            })
+                        }
+                    }
+                    VStack {
+                        Button(action: {
+                            print("Submit pressed")
+                            print(viewModel.answersToBeSubmitted)
+                        }) {
+                            Text("Submit")
+                                .font(.custom("Lato-Bold", size: 16))
+                                .foregroundColor(.white)
+                                .padding()
+                                .frame(width: 200, height: 40)
+                                .background(Color(red: 135/255, green: 62/255, blue: 242/255))
+                                .cornerRadius(20)
+                        }
+                        .frame(maxWidth: .infinity)
+                    }
+                    .frame(maxWidth: .infinity)
+                } else {
+                    Text("No Quests availabke")
                 }
-                .frame(maxWidth: .infinity)
             }
-            .frame(maxWidth: .infinity)
         }
+    }
+    
+    func questsForLongForm() -> [LongQuest]? {
+        var longQuest: [LongQuest]?
+        
+        switch elementType {
+        case .sidewalk:
+            longQuest = QuestsRepository.shared.sideWalkLongQuestModel?.quests
+        case .kerb:
+            longQuest = QuestsRepository.shared.kerbLongQuestModel?.quests
+        case .crossing:
+            longQuest = QuestsRepository.shared.crossingsLongQuestModel?.quests
+        case .none:
+            print("None")
+        }
+        return longQuest
     }
 }
 
 #Preview {
     LongForm()
+}
+
+
+enum LongFormElementType {
+    case sidewalk,kerb,crossing
 }
