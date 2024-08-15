6dd7961e180315d10b4ff4695d4647beb193ca7b
Achyut Kumar M <achyutm@gaussiansolutions.com>
2024-08-16 04:12:50 +0530

    implement APIEndpoint struct

--- GoInfoGame/GoInfoGame/Network/APIEndPoint.swift
+++ GoInfoGame/GoInfoGame/Network/APIEndPoint.swift
@@ -6,3 +6,23 @@
 //
 
 import Foundation
+
+struct APIEndpoint {
+    let path: String
+    let method: String
+    let body: Data?
+    let headers: [String: String]?
+    
+    
+    static let login = { (loginParams:Data) in APIEndpoint(path: "/authenticate", method: "POST", body: loginParams, headers: nil) }
+    
+    static let fetchWorkspaceList = APIEndpoint(path: "/workspaces/mine", method: "GET", body: nil, headers: nil)
+    
+    static let fetchLongQuests = { (workspaceId: String) in APIEndpoint(path: "/workspaces/\(workspaceId)/quests/long", method: "GET", body: nil, headers: nil) }
+    
+    static let fetchOSMElements = APIEndpoint(path: <#T##String#>, method: <#T##String#>, body: <#T##Data?#>, headers: <#T##[String : String]?#>)
+    
+    
+    
+
+}
