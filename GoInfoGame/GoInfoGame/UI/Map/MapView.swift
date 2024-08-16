c0b5832dd906d86b7667c825226d3a18f2c411a4
shweta2101 <shwethap@vindago.in>
2024-08-16 19:25:03 +0530

    enabled dismiss after submission

--- GoInfoGame/GoInfoGame/UI/Map/MapView.swift
+++ GoInfoGame/GoInfoGame/UI/Map/MapView.swift
@@ -113,7 +113,7 @@ struct MapView: View {
         })
         .onReceive(MapViewPublisher.shared.dismissSheet) { scenario in
             
-//            isPresented = false
+            isPresented = false
             switch scenario {
             case .dismissed:
                 shouldShowPolyline = false
