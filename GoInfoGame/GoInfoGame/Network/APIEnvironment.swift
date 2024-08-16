d3f95dd98e473a6b4e29fd25c19077406b85da0d
Achyut Kumar M <achyutm@gaussiansolutions.com>
2024-08-16 22:30:04 +0530

    add user profile to env, config and endpoint

--- GoInfoGame/GoInfoGame/Network/APIEnvironment.swift
+++ GoInfoGame/GoInfoGame/Network/APIEnvironment.swift
@@ -51,4 +51,17 @@ enum APIEnvironment {
             return ""
         }
     }
+    
+    var userProfileBaseURL: String {
+        switch self {
+        case .development:
+            return "https://tdei-usermanagement-be-dev.azurewebsites.net/api/v1"
+        case .staging:
+            return "https://tdei-gateway-stage.azurewebsites.net/api/v1"
+        case .production:
+            return ""
+        case .osm:
+            return ""
+        }
+    }
 }
