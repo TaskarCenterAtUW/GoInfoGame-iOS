e6b8b30bb3da2f51037bad15ba716cfe1f15fa2b
Achyut Kumar M <achyutm@gaussiansolutions.com>
2024-11-21 20:08:35 +0530

    add choiceFollowUp button to open the camera and upload the image to kartaview

--- GoInfoGame/GoInfoGame/quests/LongQuests/Components/QuestOptions.swift
+++ GoInfoGame/GoInfoGame/quests/LongQuests/Components/QuestOptions.swift
@@ -20,7 +20,11 @@ struct QuestOptions: View {
     @State private var textFieldValue: String = ""
     
     @Binding var currentAnswer: String?
-       
+    
+    var uploadPhoto: (Bool) -> ()
+    
+    @State private var isCameraPresented = false
+
     var body: some View {
                 
         switch questType {
@@ -66,9 +70,31 @@ struct QuestOptions: View {
                                     .cornerRadius(25)
                                 
                                 if option.choiceFollowUp != nil && currentAnswer == option.value {
-                                    Text(option.choiceFollowUp ?? "NOTHING HERE")
-                                            .font(.custom("Lato-Bold", size: 13))
-                                            .foregroundStyle(.gray)
+                                    HStack {
+                                        Button(action: {
+                                            uploadPhoto(true)
+                                        }) {
+                                            HStack(spacing: 8) {
+                                                Image(systemName: "camera")
+                                                    .font(.system(size: 14, weight: .medium))
+                                                    .foregroundColor(.white)
+                                                
+                                                Text(option.choiceFollowUp ?? "Upload a picture")
+                                                    .font(.custom("Lato-Regular", size: 13))
+                                                    .foregroundColor(.white)
+                                            }
+                                            .padding(.horizontal, 16)
+                                            .padding(.vertical, 8)
+                                            .background(
+                                                LinearGradient(gradient: Gradient(colors: [Color.blue, Color.purple]), startPoint: .leading, endPoint: .trailing)
+                                            )
+                                            .cornerRadius(8)
+                                            .shadow(color: Color.gray.opacity(0.5), radius: 4, x: 2, y: 2)
+                                        }
+                                    }
+                                    .padding(.horizontal, 16)
+                                    .frame(maxWidth: .infinity)
+
                                 }
                                 
                             }
