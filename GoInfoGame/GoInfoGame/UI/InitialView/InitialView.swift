d1abf124fa1b25a473879a8eb4da3125998e0f24
Achyut Kumar M <achyutm@gaussiansolutions.com>
2024-12-19 13:20:45 +0530

    - show loading indicator while fetching workspaces - show up 'no workspaces available" label for zero count

--- GoInfoGame/GoInfoGame/UI/InitialView/InitialView.swift
+++ GoInfoGame/GoInfoGame/UI/InitialView/InitialView.swift
@@ -11,47 +11,57 @@ struct InitialView: View {
     @StateObject private var viewModel = InitialViewModel()
     @State private var shouldNavigateToMapView = false
     @StateObject private var locManagerDelegate = LocationManagerDelegate()
-
+    
+    @State private var isLoading: Bool = false
+    
+    @AppStorage("loggedIn") private var loggedIn: Bool = false
+    
     var body: some View {
         NavigationStack {
-            if locManagerDelegate.isLocationServicesOff || locManagerDelegate.isLocationDenied {
-                LocationDisabledView()
-            } else {
-                if viewModel.workspaces.count > 1 {
-                    WorkspacesListView(workspaces: viewModel.workspaces, viewModel: viewModel)
-                } else if viewModel.workspaces.count == 1 {
-                    if let selectedWorkspace = viewModel.workspaces.first {
-                        VStack {
-                            if !shouldNavigateToMapView {
-                                ActivityView(activityText: "Fetching workspace data...")
-                            }
-                        }
-                        .onAppear {
-                            viewModel.fetchLongQuestsFor(workspaceId: "\(selectedWorkspace.id)") { success in
-                                if success {
-                                    let workspaceId = "\(selectedWorkspace.id)"
-                                    _ = KeychainManager.save(key: "workspaceID", data: workspaceId)
-                                    DispatchQueue.main.async {
-                                        self.shouldNavigateToMapView = true
-                                    }
-                                }
-                            }
-                        }
-                        .navigationDestination(isPresented: $shouldNavigateToMapView) {
-                            MapView(selectedWorkspace: selectedWorkspace)
-                                .navigationBarBackButtonHidden(true)
-                        }
-                    }
-                } else {
-                    ActivityView(activityText: "Looking for workspaces...")
+            ZStack {
+                VStack {
+                       HStack {
+                           NavigationLink(destination: UserProfileView()) {
+                               Image(systemName: "person.crop.circle.fill")
+                                   .resizable()
+                                   .frame(width: 27, height: 27)
+                                   .padding([.leading], 18)
+                                
+                           }
+                           Spacer()
+                       }
+                       .frame(maxWidth: .infinity, alignment: .leading)
+                       
+                       Spacer()
+                       
+                       VStack(spacing: 30) {
+                           Image("osmlogo")
+                               .resizable()
+                               .frame(width: 100, height: 100)
+                           Text("GoInfoGame")
+                               .font(.system(size: 30, design: .rounded))
+                       }
+                       .padding()
+                  
+                       Spacer()
+                       
+      if viewModel.workspaces.count > 1 {
+          WorkspacesListView(workspaces: viewModel.workspaces, viewModel: viewModel, isLoading: $isLoading)
+                       }
+                   }
+                .padding()
+                
+                if isLoading {
+                    ActivityView(activityText: "Fetching workspaces")
+                       
                 }
             }
         }
         .onAppear {
-            viewModel.locationManagerDelegate.requestLocationAuthorization()
+            isLoading = true
         }
-        .navigationBarBackButtonHidden(true)
-    }
+        .toolbar(.hidden)
+       }
 }
 
 // WorkspacesListView - View for displaying a list of workspaces
@@ -61,51 +71,87 @@ struct WorkspacesListView: View {
     @State private var shouldNavigateToMapView = false
     @State private var selectedWorkspace: Workspace?
     
-    @AppStorage("loggedIn") private var loggedIn: Bool = false
-
+    @Binding var isLoading: Bool
+    
     var body: some View {
-        ZStack(alignment: .topLeading) {
-            VStack(alignment: .leading) {
-                VStack(spacing: 30) {
-                    Image("osmlogo")
-                        .resizable()
-                        .frame(width: 100, height: 100)
-                    Text("GoInfoGame")
-                        .font(.system(size: 30, design: .rounded))
-                    Text("Pick the workspace you want to contribute to")
-                        .font(.system(size: 16, design: .rounded))
-                        .foregroundStyle(.gray)
+        
+        if viewModel.workspaces.count == 1 {
+            if let selectedWorkspace = viewModel.workspaces.first {
+                VStack {
+                    if !shouldNavigateToMapView {
+                        ActivityView(activityText: "Fetching workspace data...")
+                        Spacer()
+                    }
                 }
-                .padding()
-                ScrollView {
-                    VStack(spacing: 20) {
-                        ForEach(workspaces.filter({$0.type == "osw"}), id: \.id) { workspace in
-                            Button {
-                                viewModel.fetchLongQuestsFor(workspaceId: "\(workspace.id)", completion: { success in
-                                    if success {
-                                        self.shouldNavigateToMapView = true
-                                        self.selectedWorkspace = workspace
-                                        
-                                        let workspaceId = "\(workspace.id)"
-                                        _ = KeychainManager.save(key: "workspaceID", data: workspaceId)
-                                    }
-                                })
-                            }  label: {
-                                Text(workspace.title)
-                                    .font(.system(size: 17))
-                                    .frame(maxWidth: .infinity, maxHeight: 40)
+                .onAppear {
+                    viewModel.fetchLongQuestsFor(workspaceId: "\(selectedWorkspace.id)") { success in
+                        if success {
+                            let workspaceId = "\(selectedWorkspace.id)"
+                            _ = KeychainManager.save(key: "workspaceID", data: workspaceId)
+                            DispatchQueue.main.async {
+                                self.shouldNavigateToMapView = true
                             }
-                            .font(.custom("Lato-Bold", size: 25))
-                            .foregroundColor(Color.white)
-                            .padding()
-                            .background(Color(red: 135/255, green: 62/255, blue: 242/255))
-                            .buttonBorderShape(.roundedRectangle(radius: 10))
                         }
                     }
                 }
+                .navigationDestination(isPresented: $shouldNavigateToMapView) {
+                    MapView(selectedWorkspace: selectedWorkspace)
+                        .navigationBarBackButtonHidden(true)
+                }
+            }
+        } else if viewModel.workspaces.count == 0 {
+            VStack {
+                Text("No workspaces available for you to work on.")
+                    .font(.custom("Lato-Bold", size: 20))
+                    .foregroundColor((Color(red: 135/255, green: 62/255, blue: 242/255)))
+                    .multilineTextAlignment(.center)
+                    .onAppear {
+                        isLoading = false
+                    }
+                Spacer()
+            }
+           
+        } else {
+            VStack {
+                Text("Pick the workspace you want to contribute to")
+                    .onAppear {
+                        isLoading = false
+                    }
+                    .font(.system(size: 16, design: .rounded))
+                    .foregroundStyle(.gray)
+                
+                ScrollView {
+                            VStack(spacing: 20) {
+                                ForEach(workspaces.filter({$0.type == "osw"}), id: \.id) { workspace in
+                                    Button {
+                                        viewModel.fetchLongQuestsFor(workspaceId: "\(workspace.id)", completion: { success in
+                                            if success {
+                                                self.shouldNavigateToMapView = true
+                                                self.selectedWorkspace = workspace
+                                                
+                                                let workspaceId = "\(workspace.id)"
+                                                _ = KeychainManager.save(key: "workspaceID", data: workspaceId)
+                                            }
+                                        })
+                                    }  label: {
+                                        Text(workspace.title)
+                                            .font(.system(size: 17))
+                                            .frame(maxWidth: .infinity, maxHeight: 40)
+                                    }
+                                    .font(.custom("Lato-Bold", size: 25))
+                                    .foregroundColor(Color.white)
+                                    .padding()
+                                    .background(Color(red: 135/255, green: 62/255, blue: 242/255))
+                                    .buttonBorderShape(.roundedRectangle(radius: 10))
+                                }
+                            }
+                        }
+                .onAppear {
+                    
+                }
                 .padding()
             }
-            .padding()
+        }
 
             if shouldNavigateToMapView, let selectedWorkspace = selectedWorkspace {
                 NavigationLink(value: selectedWorkspace) {
@@ -116,19 +162,6 @@ struct WorkspacesListView: View {
                         .navigationBarBackButtonHidden(true)
                 }
             }
-            
-                NavigationLink(destination: UserProfileView()) {
-                    Image(systemName: "person.crop.circle.fill")
-                        .resizable()
-                        .frame(width: 27, height: 27)
-                        .padding([.leading], 18)
-                     
-                }
-            
-             
-
-            
-        }
     }
 }
 
@@ -163,7 +196,7 @@ struct LocationDisabledView: View {
 }
 
 #Preview {
-    LocationDisabledView()
+    InitialView()
     
 }
 
