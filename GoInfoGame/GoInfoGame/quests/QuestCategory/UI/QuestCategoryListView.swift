ebde5b95902c9ccf6f5c1b164c6a6e60f78951e3
Achyut Kumar M <achyutm@gaussiansolutions.com>
2025-01-16 23:38:28 +0530

    QuestCategoryListView UI

--- GoInfoGame/GoInfoGame/quests/QuestCategory/UI/QuestCategoryListView.swift
+++ GoInfoGame/GoInfoGame/quests/QuestCategory/UI/QuestCategoryListView.swift
@@ -8,11 +8,48 @@
 import SwiftUI
 
 struct QuestCategoryListView: View {
+    @ObservedObject var questManager = QuestsRepository.shared
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack {
+            List {
+                ForEach(questManager.allQuests.indices, id: \.self) { index in
+                    HStack {
+                        Image(uiImage: questManager.allQuests[index].quest.icon)
+                            .resizable()
+                            .aspectRatio(contentMode: .fit)
+                            .frame(width: 30, height: 30)
+                        
+                        Text(questManager.allQuests[index].quest.title)
+                        
+                        Button(action: {
+                            questManager.allQuests[index].toggleIsDefault()
+                        }) {
+                            CheckBoxView(isChecked: questManager.allQuests[index].isDefault)
+                        }
+                    }
+                }
+            }
+            .navigationTitle("Quest Selection")
+            .onDisappear {
+                QuestsPublisher.shared.refreshQuest.send("")
+            }
+        }
     }
 }
+
+struct CheckBoxView: View {
+    
+    let isChecked: Bool;
+    
+    var body: some View {
+        Image(systemName: isChecked ? "checkmark.square.fill" : "square")
+            .foregroundColor(isChecked ? Color(UIColor.systemBlue) : Color.secondary)
+    }
+}
+
 
 #Preview {
     QuestCategoryListView()
 }
+
