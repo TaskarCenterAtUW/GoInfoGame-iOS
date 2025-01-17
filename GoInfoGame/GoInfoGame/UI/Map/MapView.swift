a01ce3305b3e8e8f68fc65819b3d8421ae768d5b
Achyut Kumar M <achyutm@gaussiansolutions.com>
2025-01-17 21:18:40 +0530

    remove polyline on dismissing popover

--- GoInfoGame/GoInfoGame/UI/Map/MapView.swift
+++ GoInfoGame/GoInfoGame/UI/Map/MapView.swift
@@ -122,29 +122,31 @@ struct MapView: View {
             .toolbarBackground(.visible, for: .navigationBar)
         
             .popover(isPresented: $showPopover) {
-                            VStack {
-                                Button("Hide Quest") {
-                                                    
-                                    viewModel.hideQuest(elementId: viewModel.selectedQuest!.parent!.displayUnit.id)
-                                    showPopover = false
-                                    shouldShowPolyline = false
-                                }
-                                .padding()
-                                
-                                Button("Answer Quest") {
-                                    showPopover = false
-                                    isPresented = true
-                                }
-                                .padding()
-                            }
-                            .onAppear {
-                                shouldShowPolyline = true
-                            }
-                            .frame(width: 200, height: 100)
-                            .presentationDetents([.fraction(0.5)])
-                        }
-           
-        
+                VStack {
+                    Button("Hide Quest") {
+                        viewModel.hideQuest(elementId: viewModel.selectedQuest!.parent!.displayUnit.id)
+                        showPopover = false
+                        shouldShowPolyline = false
+                    }
+                    .padding()
+                
+                    Button("Answer Quest") {
+                        showPopover = false
+                        isPresented = true
+                    }
+                    .padding()
+                }
+                .frame(maxHeight: 50)
+                .onAppear {
+                    shouldShowPolyline = true
+                }
+                .presentationDetents([.fraction(0.5)])
+            }
+            .onChange(of: showPopover) { newValue in
+                if !newValue {
+                    shouldShowPolyline = false
+                }
+            }
         .sheet(isPresented: $isPresented, content: {
             let selectedQuest = self.viewModel.selectedQuest
             CustomSheetView {
