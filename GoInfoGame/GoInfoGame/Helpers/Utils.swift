4850e068c91f5edce21a9e270206cc15e4a95f91
freelancer12exp <freelancer12exp@gmail.com>
2025-03-12 00:43:04 +0530

    clearing the UserDefaults keys

--- GoInfoGame/GoInfoGame/Helpers/Utils.swift
+++ GoInfoGame/GoInfoGame/Helpers/Utils.swift
@@ -43,5 +43,7 @@ struct Utilities {
         _ = KeychainManager.delete(key: "username")
         loggedIn = false
         UserProfileCache.shared.clearUserProfile()
+        UserDefaults.standard.removeObject(forKey: "accessToken_Generate")
+        UserDefaults.standard.removeObject(forKey: "accessToken_expire_in")
     }
 }
