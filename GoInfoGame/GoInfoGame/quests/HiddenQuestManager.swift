1b40b55a7086e83ed551110ce355068f94d33158
Achyut Kumar M <achyutm@gaussiansolutions.com>
2025-03-28 16:54:53 +0530

    add HiddenQuestManager to project

---
+++ GoInfoGame/GoInfoGame/quests/HiddenQuestManager.swift
@@ -0,0 +1,50 @@
+//
+//  HiddenQuestManager.swift
+//  GoInfoGame
+//
+//  Created by Achyut Kumar Maddela on 28/03/25.
+//
+
+import SwiftUI
+
+class HiddenQuestManager: ObservableObject {
+    static let shared = HiddenQuestManager()
+
+    @Published var hiddenQuests: [HiddenQuest] = []
+    
+    private init() {
+        loadHiddenQuests()
+    }
+
+    func loadHiddenQuests(){
+        let decoder = JSONDecoder()
+        guard let data = UserDefaults.standard.data(forKey: "hiddenElements"), let decodedData = try? decoder.decode([HiddenQuest].self, from: data)  else { return }
+        hiddenQuests = decodedData
+    }
+    
+    func hideQuest(elementId: String, elementName: String, items: inout [DisplayUnitWithCoordinate]) {
+        print(elementId)
+        
+        guard let questIdString = elementId.components(separatedBy: "-").first,
+              let questId = Int(questIdString),
+              let index = items.firstIndex(where: { $0.id == questId }) else {
+            print("Invalid elementId or quest not found.")
+            return
+        }
+        items[index].isHidden = true
+        
+        if let data = UserDefaults.standard.data(forKey: "hiddenElements") {
+            let decoder = JSONDecoder()
+            if let decodedData = try? decoder.decode([HiddenQuest].self, from: data) {
+                hiddenQuests = decodedData
+            }
+        }
+        if !hiddenQuests.contains(where: { $0.id == Int64(questId) }) {
+            hiddenQuests.append(HiddenQuest(id: Int64(questId), name: elementName))
+            let encoder = JSONEncoder()
+            if let encodedData = try? encoder.encode(hiddenQuests) {
+                UserDefaults.standard.set(encodedData, forKey: "hiddenElements")
+            }
+        }
+    }
+}
