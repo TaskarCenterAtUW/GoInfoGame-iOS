223afc72007ed8f9b49f4d3058146cf134ab3a15
Achyut Kumar M <achyutm@gaussiansolutions.com>
2025-04-21 13:31:14 +0530

    add tap gesture to dismissKeyboard

--- GoInfoGame/GoInfoGame/quests/LongQuests/View/LongForm.swift
+++ GoInfoGame/GoInfoGame/quests/LongQuests/View/LongForm.swift
@@ -214,6 +214,11 @@ struct LongForm: View, QuestForm {
                         }
                     }
                 }
+                .onTapGesture {
+                    print("DISMISS KEYBAORD")
+                    //hide keyboard if keyboard is open
+                    UIApplication.shared.sendAction(#selector(UIResponder.resignFirstResponder), to: nil, from: nil, for: nil)
+                }
             }
             .onChange(of: capturedImage) { newValue in
                 if newValue != nil {
