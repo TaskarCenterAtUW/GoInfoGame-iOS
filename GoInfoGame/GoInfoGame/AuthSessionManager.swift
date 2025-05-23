e4c015aee6d419c25567f0b6d4d70edbbba0e919
Achyut Kumar M <achyutm@gaussiansolutions.com>
2025-05-23 17:08:51 +0530

    add AuthSessionManager and RequestAdapter to project

---
+++ GoInfoGame/GoInfoGame/AuthSessionManager.swift
@@ -0,0 +1,36 @@
+//
+//  AuthSessionManager.swift
+//  GoInfoGame
+//
+//  Created by Achyut Kumar M on 23/05/25.
+//
+
+
+final class AuthSessionManager {
+    static let shared = AuthSessionManager()
+    var accessToken: String?
+    var username: String?
+    var worksapaceId: String?
+    
+    private init() {
+        accessToken = KeychainManager.load(key: "accessToken")
+        username = KeychainManager.load(key: "username")
+        worksapaceId = KeychainManager.load(key: "workspaceID")
+    }
+    
+    func updateToken(_ token: String) {
+        accessToken = token
+       _ = KeychainManager.save(key: "accessToken", data: token)
+    }
+    
+    func setUser(_ user: String) {
+        username = user
+       _ = KeychainManager.save(key: "username", data: user)
+    }
+    
+    func setWorkspaceId(_ workspaceId: String) {
+        worksapaceId = workspaceId
+        _ = KeychainManager.save(key: "workspaceID", data: workspaceId)
+    }
+}
+
