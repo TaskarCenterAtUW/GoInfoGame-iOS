2ddf9942c1abd6e93fc7123843267621794e8055
prateeka <prateekan6@gmail.com>
2025-05-28 16:00:08 +0530

    removed dead code in AppQuestManager

--- GoInfoGame/GoInfoGame/AppQuestManager.swift
+++ GoInfoGame/GoInfoGame/AppQuestManager.swift
@@ -9,7 +9,6 @@ import Foundation
 //import SwiftOverpassAPI
 import osmparser
 import MapKit
-import osmapi
 import RealmSwift
 
 
@@ -22,49 +21,6 @@ class AppQuestManager {
     
     static let shared = AppQuestManager()
     private init() {}
-    
-    private let seedBox = BBox(minLat: 47.70312160869372, maxLat: 47.718964653825054, minLon: -122.20866792353317, maxLon: -122.18570621653987)
-    
-    func getUpdatedQuest(elementId: String) -> DisplayUnitWithCoordinate? {
-        let nodesFromStorage = dbInstance.getNodes().filter { n in
-            n.tags.count != 0
-        }
-        let waysFromStorage = dbInstance.getWays().filter{w in w.tags.count != 0 }
-        let theWay = waysFromStorage.first(where: {$0.id == Int(elementId)})
-        let theNode = nodesFromStorage.first(where: {$0.id == Int(elementId)})
-        let allQuests = QuestsRepository.shared.applicableQuests
-        // theElement if not equal to nil
-        if (theNode != nil) {
-            let nodeElement = theNode?.asNode()
-            for quest in allQuests {
-                    if quest.quest.filter.isEmpty {continue} // Ignore quest
-                    if quest.quest.isApplicable(element: nodeElement!){
-                        // Create a duplicate of the quest
-                        // Create a display Unit
-                        let duplicateQuest = quest.quest.copyWithElement(element: nodeElement!)
-                        let unit = DisplayUnitWithCoordinate(displayUnit: duplicateQuest.displayUnit, coordinateInfo:  CLLocationCoordinate2D(latitude: nodeElement!.position.latitude, longitude: nodeElement!.position.longitude), id: nodeElement!.id, isHidden: false)
-                        return unit
-                    }
-            }
-            
-        }
-        else if (theWay != nil){
-            let wayElement = theWay?.asWay()
-            for quest in allQuests {
-                    if quest.quest.filter.isEmpty {continue} // Ignore quest
-                    if quest.quest.isApplicable(element: wayElement!){
-                        // Create a duplicate of the quest
-                        // Need to add another here.
-                        let duplicateQuest = quest.quest.copyWithElement(element: wayElement!)
-                        let position  = dbInstance.getCenterForWay(id: String(wayElement!.id)) ?? CLLocationCoordinate2D()
-                        let unit = DisplayUnitWithCoordinate(displayUnit: duplicateQuest.displayUnit, coordinateInfo: position, id: wayElement!.id, isHidden: false)
-                    return unit
-                    }
-            }
-            
-        }
-        return nil
-    }
 
     // Fetches all the available quests from Database
     func fetchQuestsFromDB() ->  [DisplayUnitWithCoordinate] {
