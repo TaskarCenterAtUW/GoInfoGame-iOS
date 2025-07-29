4e9933d20e60af2fd386ab12b0c30363c1536876
SrikanthV <srikanthv@vindago.in>
2025-07-29 17:32:00 +0530

    Updated message to display right message before loading the workspaces

--- GoInfoGame/GoInfoGame/UI/InitialView/InitialViewModel.swift
+++ GoInfoGame/GoInfoGame/UI/InitialView/InitialViewModel.swift
@@ -14,7 +14,7 @@ import LocalAuthentication
 @MainActor
 class InitialViewModel: ObservableObject {
     let locationManagerDelegate = LocationManagerDelegate()
-    @Published var workspaces: [Workspace] = []
+    @Published var workspaces: [Workspace]? = nil
     @Published var longQuests: [LongFormElement] = []
     @Published var isLoading: Bool = false
     
