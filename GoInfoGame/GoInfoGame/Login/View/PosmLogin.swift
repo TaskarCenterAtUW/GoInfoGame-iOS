a36198a1fad3dee90aa24da832244cc6aff17200
SrikanthV <srikanthv@vindago.in>
2025-09-18 15:21:45 +0530

    reverted debug mode app changes

--- GoInfoGame/GoInfoGame/Login/View/PosmLogin.swift
+++ GoInfoGame/GoInfoGame/Login/View/PosmLogin.swift
@@ -174,17 +174,6 @@ struct PosmLoginView: View {
                 } message: {
                     Text("Your session has expired. Please login again")
                 }
-        #if DEBUG
-                .onAppear {
-                    if let windowScene = UIApplication.shared.connectedScenes
-                        .first(where: { $0.activationState == .foregroundActive }) as? UIWindowScene {
-                        if let debug = windowScene.keyWindow?.viewWithTag(100) as? UIView {
-                            windowScene.keyWindow?.bringSubviewToFront(debug)
-                        }
-                    }
-                    
-                }
-        #endif
     }
     
     var appVersionText: Text {
--- GoInfoGame/GoInfoGame/SceneDelegate.swift
+++ GoInfoGame/GoInfoGame/SceneDelegate.swift
@@ -41,27 +41,6 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
                 window.makeKeyAndVisible()
             }
         }
-#if DEBUG
-        if let window = self.window {
-            // add the label left bottom of the screen
-            let debugLabel = UILabel()
-            debugLabel.text = "DEBUG"
-            debugLabel.tag = 100
-            debugLabel.textAlignment = .center
-            debugLabel.font = .boldSystemFont(ofSize: 12)
-            debugLabel.textColor = .white
-            debugLabel.backgroundColor = Asset.Colors.accentPink.color
-            debugLabel.translatesAutoresizingMaskIntoConstraints = false
-            
-            window.addSubview(debugLabel)
-            NSLayoutConstraint.activate([
-                debugLabel.topAnchor.constraint(equalTo: window.safeAreaLayoutGuide.bottomAnchor, constant: 0),
-                debugLabel.widthAnchor.constraint(equalTo: window.safeAreaLayoutGuide.widthAnchor),
-                debugLabel.heightAnchor.constraint(equalToConstant: 14)
-            ])
-           
-        }
-#endif
     }
 
     func sceneDidDisconnect(_ scene: UIScene) {
