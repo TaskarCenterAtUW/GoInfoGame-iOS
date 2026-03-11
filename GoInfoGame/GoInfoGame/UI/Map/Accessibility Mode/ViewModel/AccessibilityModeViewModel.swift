e39cee55751332f45b1473d12dad1307bfc72763
SrikanthV <srikanthv@vindago.in>
2026-03-11 23:12:50 +0530

    Fixed auto selection dailog flicker issue

--- GoInfoGame/GoInfoGame/UI/Map/Accessibility Mode/ViewModel/AccessibilityModeViewModel.swift
+++ GoInfoGame/GoInfoGame/UI/Map/Accessibility Mode/ViewModel/AccessibilityModeViewModel.swift
@@ -60,9 +60,9 @@ class AccessibilityModeViewModel: ObservableObject {
         if let nearestQuest =  sortedquests.first,
            autoSelectionCanceledIDs.contains(nearestQuest.quest.id) == false,
            nearestQuest.distance <= locationAccuracy {
-            self.selectedQuest = nearestQuest.quest
-            self.mapViewModel.selectedQuest = nearestQuest.quest.displayUnit
-            if !self.isQuestAutoSelected {
+            if self.isQuestAutoSelected == false {
+                self.selectedQuest = nearestQuest.quest
+                self.mapViewModel.selectedQuest = nearestQuest.quest.displayUnit
                 self.isQuestAutoSelected = true
             }
         } else if self.isQuestAutoSelected == true {
