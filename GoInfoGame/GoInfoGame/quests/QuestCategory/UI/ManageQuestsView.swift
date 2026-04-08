42aeeb2d8c78b390bab862cffe34acdaee6f1d5d
SrikanthV <srikanthv@vindago.in>
2026-04-08 12:17:01 +0530

    Fixed accessibility issues in ManageQuestsView

--- GoInfoGame/GoInfoGame/quests/QuestCategory/UI/ManageQuestsView.swift
+++ GoInfoGame/GoInfoGame/quests/QuestCategory/UI/ManageQuestsView.swift
@@ -16,10 +16,12 @@ struct ManageQuestsView: View {
         VStack(alignment: .leading, spacing: 12) {
             HStack(alignment: .firstTextBaseline) {
                 Text("Choose which features to survey")
-                    .font(.custom("Lato-Bold", size: 20, relativeTo: .title))
+                    .font(FontFamily.Lato.bold.swiftUIFont(size: 20, relativeTo: .headline))
+                    .fixedSize(horizontal: false, vertical: true) // Prevents the "unsupported" warning
                     .foregroundColor(Asset.Colors.huskyPurple.swiftUIColor)
                     .multilineTextAlignment(.leading)
                     .padding(.top, 30)
+                    .accessibilityLabel("Choose which features to survey")
 
                 Spacer()
 
@@ -35,10 +37,11 @@ struct ManageQuestsView: View {
             .padding(.horizontal, 16)
 
             Text("Show all hidden elements on the map by individual item or type")
-                .font(.custom("Lato-Bold", size: 12, relativeTo: .subheadline))
+                .font(FontFamily.Lato.bold.swiftUIFont(size: 12, relativeTo: .subheadline))
                 .foregroundColor(Asset.Colors.huskyPurple.swiftUIColor)
                 .multilineTextAlignment(.leading)
                 .padding(.horizontal)
+                .accessibilityLabel("Show all hidden elements on the map by individual item or type")
 
             Text("FEATURES")
                 .font(.custom("Lato-Bold", size: 15, relativeTo: .headline))
@@ -46,8 +49,9 @@ struct ManageQuestsView: View {
                 .padding(.horizontal)
                 .padding(.top, 10)
                 .padding(.leading, 15)
+                .accessibilityLabel("Features")
             
-            GeometryReader { geometry in
+            ScrollView {
                 VStack(spacing: 0) { // No extra spacing between rows
                     ForEach(questManager.longQuestModels.indices, id: \.self) { index in
                         let quest = questManager.allQuests[index]
@@ -57,8 +61,11 @@ struct ManageQuestsView: View {
 
                         Toggle(isOn: $questManager.allQuests[index].isDefault) {
                             Text(title)
-                                .font(.custom("Lato-Bold", size: 16, relativeTo: .headline))
+                                .font(FontFamily.Lato.bold.swiftUIFont(size: 16, relativeTo: .headline))
                                 .foregroundColor(Asset.Colors.huskyPurple.swiftUIColor)
+                                .multilineTextAlignment(.leading)
+                                .lineLimit(nil)
+                                .accessibilityLabel(title)
                         }
                         .toggleStyle(SwitchToggleStyle(tint: Asset.Colors.accentPink.swiftUIColor))
                         .padding(.horizontal, 10)
@@ -75,7 +82,7 @@ struct ManageQuestsView: View {
                 .clipShape(RoundedRectangle(cornerRadius: 16))
                 .padding([.leading, .trailing], 22)
             }
-            .frame(height: CGFloat(questManager.allQuests.count) * 50)
+            .frame(minHeight: CGFloat(questManager.allQuests.count) * 60)
 
 
             if hiddenQuestManager.hiddenQuests.isEmpty {
@@ -88,7 +95,7 @@ struct ManageQuestsView: View {
                         .multilineTextAlignment(.leading)
                         .padding(.horizontal)
                         .padding(.top, 10)
-                        .padding(.leading, 15)
+                        .accessibilityLabel("Hidden Elements")
                     Spacer()
                     Button(action: {
                         hiddenQuestManager.removeAllHiddenQuests()
@@ -101,6 +108,7 @@ struct ManageQuestsView: View {
                             .background(Asset.Colors.huskyPurple.swiftUIColor)
                             .multilineTextAlignment(.leading)
                             .cornerRadius(10)
+                            .accessibilityLabel("Unhide All")
                     }
                     .padding(.trailing, 15)
 
@@ -111,6 +119,7 @@ struct ManageQuestsView: View {
                     .multilineTextAlignment(.leading)
                     .foregroundColor(Asset.Colors.huskyPurple.swiftUIColor)
                     .padding(.horizontal)
+                    .accessibilityLabel("Swipe left on item to show delete option and delete it from the list.")
 
                 List {
                     ForEach(hiddenQuestManager.hiddenQuests.indices, id: \.self) { index in
@@ -119,17 +128,20 @@ struct ManageQuestsView: View {
                         HStack {
                             Text("ID: \(String(quest.id))")
                                 .font(.custom("Lato-Bold", size: 15, relativeTo: .headline))
-                                .foregroundColor(Color(red: 69 / 255, green: 81 / 255, blue: 108 / 255))
-                        
+                                .fixedSize(horizontal: false, vertical: true)
+                                .accessibilityLabel("ID: \(String(quest.id))")
                             Spacer()
+                            
                             Text(quest.name)
-                                .font(.custom("Lato-Bold", size: 13, relativeTo: .subheadline))
-                                .foregroundColor(Asset.Colors.huskyPurple.swiftUIColor)
-                                
+                                .font(.custom("Lato-Bold", size: 13, relativeTo: .body))
+                                .fixedSize(horizontal: false, vertical: true)
+                                .accessibilityLabel(quest.name)
                         }
+                        .multilineTextAlignment(.leading)
+                        .lineLimit(nil)
                         .accessibilityElement(children: .combine)
-                        .accessibilityLabel("\(quest.name), ID: \(quest.id)")
                         .listRowInsets(EdgeInsets(top: 6, leading: 10, bottom: 6, trailing: 10))
+                        .accessibilityLabel("\(quest.name), ID: \(quest.id)")
                     }
                     .onDelete { indexSet in
                         hiddenQuestManager.removeQuest(atOffsets: indexSet)
